1cafddaef1d16288eea6f6101e701a2ffec6abac
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-04-28 13:45:54 +0530

    static method

--- static1.java
+++ static1.java
@@ -1,12 +1,23 @@
 public class static1 {
-    static int  add(int a,int b){
-return a+b;
-    }
-    public static void main(String[] args) {
-        {
- int res=add(2, 12);
- System.out.println(res);
-        }
-    }
+//     static int  add(int a,int b){
+// return a+b;
+//     }
+//     public static void main(String[] args) {
+//         {
+//  int res=add(2, 12);
+//  System.out.println(res);
+//         }
+//     }
     
+// }
+
+
+public int sum(int a,int b){
+    return a+b;
+}
+public static void main(String[] args) {
+    static1 obj=new static1();
+     int res=obj.sum(2,4);
+     System.out.println(res);
+}
 }
