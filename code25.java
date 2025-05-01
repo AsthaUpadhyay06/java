09a17b5ead039fbb59ff7877e9a2e36fb397c619
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-01 11:15:08 +0530

    constructor

--- code25.java
+++ code25.java
@@ -1,5 +1,5 @@
 public class code25{
     public static void main(String[] arg){
-        int a=
+        
     }
 }
---
+++ studen.java
@@ -0,0 +1,36 @@
+class student {
+
+//     String name="astha";
+//     int age=19;
+//     student( String s,int a){
+//         name=s;
+//         age=a;
+//         System.out.println(name+  " "   +age);
+//     }
+// }
+// class studen{
+
+
+// public static void main(String[] args) {
+//     student obj=new student("asth",29);
+
+// }
+// }
+
+
+String s= "fdrefrfa";
+int a=19;
+student(String s,int a){
+ this.s=s;
+ this.a=a;
+ System.out.println(s+""+a);
+}
+}
+class studen{
+    public static void main(String[] args) {
+        student obj=new student("ram", 34);
+        System.out.println(obj.s);
+        System.out.println(obj.a);
+        
+    }
+}
