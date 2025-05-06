249b8bfd0650a2ce40c7aae644a263fd283364e6
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-06 20:57:15 +0530

    rotate array by k

---
+++ arrayclass4.java
@@ -0,0 +1,23 @@
+public class arrayclass4 {
+    public static void main(String[] args) {
+        int [] arr={ 1,2,3,4,5};
+        int n=5;
+        int [] ans=new int[n];
+        int j=0;
+        
+        int k=2;
+        k=k%n;
+        int i;
+        for( i=n-k;i<n;i++){
+            ans[j++]=arr[i];
+        }
+        for(i=0;i<n-k;i++){
+            ans[j++]=arr[i];
+        }
+        // print rotated array 
+        for(i=0;i<n;i++){
+            System.out.print(ans[i]+ " ");
+        }
+
+    }
+}
---
+++ override5.java
@@ -0,0 +1,13 @@
+
+class person{
+    void displaydetails( String name,int age){
+        this.name=name;
+        
+    }
+}
+
+public class override5 {
+    public static void main(String[] args) {
+        
+    }
+}
