617bb71b9bc5fb91cca7044eaa2e383e9a1e5c47
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-07 23:25:26 +0530

    abstract class

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,31 @@
-n
+abstract class payment{
+     abstract void sum(int a);
+     
+   void success(){
+    System.out.println("success");
+   
+    }
+}
+class UpiPayment extends payment{
+    void sum(int a){
+        System.out.println("pay with cash"  +a);
+    }
+}
+class Netbanking extends payment{
+    void sum(int a){
+        System.out.println("payment via netbanking" +a);
+    }
+}
+
+public class abstractclass {
+    public static void main(String[] args) {
+        
+        payment obj=new UpiPayment();
+        obj.sum(34000);
+        obj.success();
+
+        payment obj1=new Netbanking();
+        obj1.sum(45000);
+        obj1.success();
+    }
+}
