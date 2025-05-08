0d47ad6bd615ad390675b6c9ec2e0832c0869e97
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-08 14:36:32 +0530

    8 may

---
+++ person.java
@@ -0,0 +1,8 @@
+public class person {
+    public static void main(String[] args) {
+        String name="astha";
+        void sayhello(){
+            System.out.println("hii iammm");
+        }
+    }
+}
---
+++ sum1.java
@@ -0,0 +1,8 @@
+public class sum1 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+       int sum=a+b;
+        System.out.println(sum);
+    }
+}
