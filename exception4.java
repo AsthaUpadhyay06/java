dbbbd94bd1665a7103378e687b645734cbe72b0d
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-22 13:09:39 +0530

    run time error

---
+++ exception4.java
@@ -0,0 +1,14 @@
+public class exception4 {
+    public static void main(String[] args) {
+        try {
+            String str=null;
+        System.out.println(str.length());
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+       
+    }
+}
+// run time error will occur
