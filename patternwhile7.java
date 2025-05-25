0cbb7d64f58f21c02337d2aff8bdb2d0e0e8d2c2
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-25 21:46:18 +0530

    pattern using while lopp 7

---
+++ patternwhile7.java
@@ -0,0 +1,16 @@
+public class patternwhile7 {
+    public static void main(String[] args) {
+        int i=1;
+        int n=4;
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print(i);
+                j++;
+
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
