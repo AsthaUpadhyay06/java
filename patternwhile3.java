60eb77a8039a262c83228173f0f263f9be599bbd
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-25 20:14:35 +0530

    patterns using while loop 3

---
+++ patternwhile3.java
@@ -0,0 +1,15 @@
+public class patternwhile3 {
+    public static void main(String[] args) {
+        int i=1;
+        int n=4;
+        while(i<=n){
+            int j=1;
+            while(j<=n){
+             System.out.print(j);
+             j=j+1;
+            }
+            System.out.println();
+            i=i+1;
+        }
+    }
+}
