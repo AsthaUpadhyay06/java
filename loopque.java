8e6063da6f90346c2e81fd3ec3532932344b038c
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-27 01:23:05 +0530

    break example

---
+++ loopque.java
@@ -0,0 +1,12 @@
+public class loopque {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=i;j<=5;j++){
+                if(i+j==5){
+                    break;
+                }
+                System.out.println(i+ "  "+ j);
+            }
+        }
+    }
+}
