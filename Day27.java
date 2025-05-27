e607d524e38f27d7266791e88bfcad630d71e5f9
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-27 14:35:50 +0530

    Hash Set

--- Day27.java
+++ Day27.java
@@ -1,13 +1,41 @@
-import java.util.LinkedList;
+// import java.util.LinkedList;
 
-import javax.naming.LinkLoopException;
+// import javax.naming.LinkLoopException;
+
+// public class Day27 {
+//     public static void main(String[] args) {
+//         LinkedList<Integer> arr=new LinkedList<>();
+//         for(int i=0;i<1000;i++){
+//             arr.add(i);
+
+            
+//         }
+//         //arr.add(4);
+//         System.out.println(arr);
+//         // System.out.println(arr.contains(4)); // output true
+//         // System.out.println(arr.contains(5));// output false
+//     }
+// }
+
+ import java.util.HashSet;
+// java.util.LinkedList;
+//
+//import javax.naming.LinkLoopException;
 
 public class Day27 {
     public static void main(String[] args) {
-        LinkedList<Integer> arr=new LinkedList<>();
-        arr.add(4);
-        System.out.println(arr.contains(4)); // output true
-        System.out.println(arr.contains(5));// output false
+        HashSet<Integer>set=new HashSet();
+        set.add(5);
+        set.add(5);
+        set.add(6);
+        System.out.println(set);
+
+            
+        }
+        //arr.add(4);
+       // System.out.println(arr);
+        // System.out.println(arr.contains(4)); // output true
+        // System.out.println(arr.contains(5));// output false
     }
-}
+
 
