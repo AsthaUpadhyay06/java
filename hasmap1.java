85e4d5ce66f17e99db28cb831045bcfbf85b0459
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-28 13:23:19 +0530

    Hash Map example count occurance

--- hasmap1.java
+++ hasmap1.java
@@ -6,7 +6,15 @@ public class hasmap1 {
         map.put("id", 101);
         map.put("astha", 619);
         map.put("astha", 678);
+        map.forEach((key,value)->{
+            System.out.println(key+ " "+value);
+        });
        // System.out.println(map.get("astha"));
-       System.out.println(map);
+    //    System.out.println(map.get("anaya"));
+    //    System.out.println(map.getOrDefault("astha", 0));
+    //    System.out.println(map.keySet());
+    //    System.out.println(map.values());
+    //    System.out.println(map);
+
     }
 }
