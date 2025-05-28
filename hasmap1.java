177627fd84ac9900fcf0ef6f7cced8ec997af8c9
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-28 12:39:11 +0530

    HashMap Example

---
+++ hasmap1.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class hasmap1 {
+    public static void main(String[] args) {
+        HashMap<String,Integer>map=new HashMap<>();
+        map.put("id", 101);
+        map.put("astha", 619);
+        map.put("astha", 678);
+       // System.out.println(map.get("astha"));
+       System.out.println(map);
+    }
+}
