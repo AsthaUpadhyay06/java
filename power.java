5d96e4370325dc183e5e8e4cf66afeff632b4979
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-28 20:30:34 +0530

    find power

---
+++ power.java
@@ -0,0 +1,17 @@
+public class power {
+
+    public int calculatepower(int a,int b){
+        int ans=1;
+        for(int i=1;i<=b;i++){
+           ans=ans*a;
+        }
+        
+        return  ans;
+
+    }
+    public static void main(String[] args) {
+        power obj=new power();
+      int result=  obj.calculatepower(2, 3);
+        System.out.println(result);
+    }
+}
