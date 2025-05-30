4e68de67b9977d48c0400b1925cda21376840134
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-30 10:15:55 +0530

    Iterator

---
+++ iteratorrs.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class iteratorrs {
+    public static void main(String[] args) {
+        ArrayList<String>names=new ArrayList<>();
+        names.add("Astha");
+         names.add("Aanaya");
+          names.add("Aarya");
+           names.add("Vanshika");
+            names.add("Harshita");
+            Iterator<String>it=names.iterator();
+            while(it.hasNext()){
+                String elem=it.next();
+                System.out.println(elem);
+                if(elem.startsWith("A")){
+                it.remove(); 
+                }
+            }
+
+
+            System.out.println(names);
+
+    }
+}
