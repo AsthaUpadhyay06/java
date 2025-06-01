d8e9eaa1bc85f7773c49ab930d2763d3260e6e74
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-01 17:02:14 +0530

    Unique Element XOR

---
+++ UniqueElementXOR.java
@@ -0,0 +1,12 @@
+ public class UniqueElementXOR {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 2, 1}; // Example array
+        int unique = 0;
+
+        for (int num : numbers) {
+            unique ^= num; // XOR operation
+        }
+
+        System.out.println("Unique element: " + unique);
+    }
+}
