4a122b10056ead2396be3d564166a219a25aa681
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-03 12:59:18 +0530

    thread sleep method

--- Multithreading.java
+++ Multithreading.java
@@ -3,6 +3,12 @@
 class MyThread extends Thread{
     public void run(){
         System.out.println("hello");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        System.out.println("astha upadhyay");
     }
 
 }
@@ -10,6 +16,7 @@ public class Multithreading {
     public static void main(String[] args) {
         MyThread obj=new MyThread();
         obj.start();
+       
         System.out.println("astha");
         
     }
