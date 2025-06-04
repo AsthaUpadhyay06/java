d9871b5b84de50cabbe05787a1a88979fb8f0a00
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-04 13:55:52 +0530

    synchronize

--- runnable2.java
+++ runnable2.java
@@ -5,38 +5,50 @@ class WithdrawThread extends Thread{
 this.amount=amount;
  }
  public void run(){
-    if(TotalBalance>=amount){
-        System.out.println("withdraw successfull " + amount );
-        
-    
-    try {
-         
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        }
-        TotalBalance=TotalBalance-amount;
-        System.out.println(" remaining balance "+ TotalBalance );
-        
-    }
-    else{
-        System.out.println("insufficient balance" );
-    }
- }
+   
+        if (TotalBalance >= amount) {
+            System.out.println(Thread.currentThread().getName() + " is going to withdraw " + amount);
 
+            try { Thread.sleep(1000); } catch (InterruptedException e) {}
+
+            TotalBalance -= amount;
+            System.out.println(Thread.currentThread().getName() + " completed withdrawal. Remaining balance: " + TotalBalance);
+        } else {
+            System.out.println(Thread.currentThread().getName() + " - Insufficient balance");
+        }
+    }
 }
+   
 
 public class runnable2 {
     public static void main(String[] args) {
         WithdrawThread t1=new WithdrawThread(800);
         WithdrawThread t2=new WithdrawThread(500);
         t1.start();
-        try {
-            t1.join();
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+        
         t2.start();      
     }
     }
 
+//class WithdrawThread extends Thread {Add commentMore actions
+//     static int balance = 1000; // shared account balance
+//     int amount;
+
+//     WithdrawThread(int amount) {
+//         this.amount = amount;
+//     }
+
+//     public void run() {
+//         // ⚠️ No synchronization used here
+//         if (balance >= amount) {
+//             System.out.println(Thread.currentThread().getName() + " is going to withdraw " + amount);
+
+//             try { Thread.sleep(1000); } catch (InterruptedException e) {}
+
+//             balance -= amount;Add commentMore actions
+//             System.out.println(Thread.currentThread().getName() + " completed withdrawal. Remaining balance: " + balance);
+//         } else {
+//             System.out.println(Thread.currentThread().getName() + " - Insufficient balance");
+//         }
+//     }
+// }
