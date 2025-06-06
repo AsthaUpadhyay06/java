217e33558c03790a2d12a7d855f8efd558159ead
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-06 13:34:21 +0530

    Reduce function

---
+++ reduce1.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class reduce1 {
+    public static void main(String[] args) {
+           ArrayList<Integer>list=new ArrayList<>();
+        list.add(4);
+        list.add(45);
+        list.add(43);
+        list.add(42);
+        list.add(43);
+        int sum=list.stream().reduce(0, (a,b)->a+b);
+        System.out.println(sum);
+    }
+
+}
---
+++ stringapi1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class stringapi1 {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(4);
+        list.add(45);
+        list.add(43);
+        list.add(42);
+        list.add(43);
+
+    }
+}
