0de91bcb408837aca9c2ddd87541b3894a4f2ad6
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-06 13:41:10 +0530

    Max using reduce method

--- reduce1.java
+++ reduce1.java
@@ -8,8 +8,9 @@ public class reduce1 {
         list.add(43);
         list.add(42);
         list.add(43);
-        int sum=list.stream().reduce(0, (a,b)->a+b);
+        int sum=list.stream().reduce(0,(a,b)-> a>b ? a:b ) ;
         System.out.println(sum);
     }
 
 }
+ // condition ? value_if_true : value_if_false;
