3e7083f8b7376b24e36ac75cfbfd1abe65f0918b
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-08 22:52:19 +0530

    three sum closet

--- hashset5.java
+++ hashset5.java
@@ -1,4 +1,7 @@
 import java.util.HashSet;
+import java.util.Iterator;
+
+
 
 public class hashset5 {
     public static void main(String[] args) {
@@ -8,6 +11,7 @@ public class hashset5 {
 
         set.add(45);
         set.add(56);
+        System.out.println(set.size());
         System.out.println(set);
         if(set.contains(34)){
             System.out.println("find");
@@ -15,6 +19,10 @@ public class hashset5 {
         if(!set.contains(46)){
             System.out.println("not find");
         }
+      Iterator it=set.iterator();
+      while(it.hasNext()){
+        System.out.println(it.next());
+      }
     }
     
 }
