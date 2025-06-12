96815466c5e717f3c0f96a7c6f472b178b8410d4
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-13 02:00:58 +0530

    Binary search problem square root

---
+++ squareroot.java
@@ -0,0 +1,29 @@
+public class squareroot {
+public static int squarerootproblem(int n){
+ 
+        int s=0;
+        int e=n;
+        int ans=-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            long square=1L*m*m;
+            if(square==n){
+                return m;
+            }
+            else if(m<n){
+                ans=m;
+                s=m+1;
+            }
+            else{
+                e=m-1;
+            }
+}
+return ans;
+}
+    public static void main(String[] args) {
+      int result= squarerootproblem(36);
+      System.out.println(result); 
+            }
+        }
+    
+
