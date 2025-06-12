8b8d7dee5c2096c8be0f0069ed3e621f09aef40d
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-12 21:18:02 +0530

    code

---
+++ code4.java
@@ -0,0 +1,17 @@
+public class code4 {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        int s=0;
+        int e=arr.length-1;
+        while(s<e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+        for(int num:arr){
+            System.out.println(num);
+        }
+    }
+}
