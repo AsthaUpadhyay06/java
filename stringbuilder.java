e0ed0f66f584e03ee38c35956b2e39465ce81da2
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-15 12:32:53 +0530

    Reverse

---
+++ stringbuilder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class stringbuilder {
+    public static void main(String[] args) {
+        
+    
+    StringBuilder sc=new StringBuilder("astha");
+    sc.append("upadhyay");
+    System.out.println(sc);
+    System.out.println(sc.reverse());
+    sc.delete(0, 7);
+    System.out.println(sc);
+    
+    }
+    
+}
