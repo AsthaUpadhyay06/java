1cda9795e1bd1f0b003f31743782bd3e5ae11d93
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-19 23:45:30 +0530

    Subset

---
+++ SubsetSum.java
@@ -0,0 +1,19 @@
+
+    public class SubsetSum {
+    public static boolean isSubsetSum(int[] arr, int n, int sum) {
+        if (sum == 0) return true;
+        if (n == 0) return false;
+
+        if (arr[n - 1] > sum)
+            return isSubsetSum(arr, n - 1, sum);
+
+        return isSubsetSum(arr, n - 1, sum) || isSubsetSum(arr, n - 1, sum - arr[n - 1]);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 34, 4, 12, 5, 2};
+        int sum = 9;
+        System.out.println(isSubsetSum(arr, arr.length, sum) ? "Yes" : "No");
+    }
+}
+
