d7f21489f641b1c2fa04218e6b674cff3b93e154
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-20 23:26:27 +0530

    CountInversions

---
+++ CountInversions.java
@@ -0,0 +1,41 @@
+public public class CountInversions {
+    public static int countInversions(int[] arr) {
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+
+    static int mergeSort(int[] arr, int l, int r) {
+        int count = 0;
+        if (l < r) {
+            int m = (l + r) / 2;
+            count += mergeSort(arr, l, m);
+            count += mergeSort(arr, m + 1, r);
+            count += merge(arr, l, m, r);
+        }
+        return count;
+    }
+
+    static int merge(int[] arr, int l, int m, int r) {
+        int[] left = java.util.Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = java.util.Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l, swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+        return swaps;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        System.out.println("Inversion Count: " + countInversions(arr));
+    }
+} 
