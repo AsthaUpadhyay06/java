f82fb2b50806bc41a17f825ed8a8728952ac3774
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-22 23:43:50 +0530

    RowColumnSum

---
+++ RowColumnSum.java
@@ -0,0 +1,27 @@
+ public class RowColumnSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Row sums
+        for (int i = 0; i < matrix.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                rowSum += matrix[i][j];
+            }
+            System.out.println("Row " + i + " sum = " + rowSum);
+        }
+
+        // Column sums
+        for (int j = 0; j < matrix[0].length; j++) {
+            int colSum = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                colSum += matrix[i][j];
+            }
+            System.out.println("Column " + j + " sum = " + colSum);
+        }
+    }
+}
