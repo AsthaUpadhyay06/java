8338613ff3293e832120f29bf359f177eed573ea
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-26 23:40:49 +0530

    Removing Spaces

---
+++ RemoveSpaces.java
@@ -0,0 +1,7 @@
+ public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Java is fun to learn";
+        String result = str.replace(" ", "");
+        System.out.println("After removing spaces: " + result);
+    }
+}
