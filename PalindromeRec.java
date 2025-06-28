ad4794c626d9c3ba4ce23c14f4107e095411010d
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-06-28 23:38:34 +0530

    PalindromeRec

---
+++ PalindromeRec.java
@@ -0,0 +1,12 @@
+ public class PalindromeRec {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if(start >= end) return true;
+        if(str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String s = "madam";
+        System.out.println(isPalindrome(s, 0, s.length() - 1) ? "Palindrome" : "Not Palindrome");
+    }
+} 
