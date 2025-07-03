f76d1989d1e462862141b5b4ccac851dcea87fb4
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-03 23:45:08 +0530

    FirstUniqueChar

---
+++ FirstUniqueChar.java
@@ -0,0 +1,13 @@
+public class FirstUniqueChar {
+    public static void main(String[] args) {
+        String str = "swiss";
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (str.indexOf(c) == str.lastIndexOf(c)) {
+                System.out.println("First non-repeating: " + c);
+                break;
+            }
+        }
+    }
+}
