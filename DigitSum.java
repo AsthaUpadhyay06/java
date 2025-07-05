337c889bc66652d013a62f6eaf9d5d6bc5219d37
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-05 21:50:16 +0530

    DigitSum

---
+++ DigitSum.java
@@ -0,0 +1,11 @@
+public class DigitSum {
+    static int sumDigits(int n) {
+        if (n == 0) return 0;
+        return n % 10 + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        int num = 1234;
+        System.out.println("Sum of digits: " + sumDigits(num));
+    }
+}
