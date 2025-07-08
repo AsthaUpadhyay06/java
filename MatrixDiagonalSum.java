5d25e9d54ae988b5de135fb3ef0cfb5dbbe5fe6d
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-08 22:47:30 +0530

    MatrixDiagonalSum

---
+++ MatrixDiagonalSum.java
@@ -0,0 +1,19 @@
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int sum = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i]; // Main diagonal
+            if (i != mat.length - 1 - i) {
+                sum += mat[i][mat.length - 1 - i]; // Secondary diagonal
+            }
+        }
+
+        System.out.println("Diagonal sum: " + sum);
+    }
+}
