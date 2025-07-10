cb6b78e7175f90182ec61ef12bbc4b50a6c17e22
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-10 23:25:08 +0530

    hashmap10

---
+++ hashmap10.java
@@ -0,0 +1,10 @@
+ import java.util.HashMap;
+
+public class hashmap10{
+  public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("John", 25);
+        map.put("Alice", 30);
+        System.out.println(map.values()); // Output: [25, 30]
+    }
+}
