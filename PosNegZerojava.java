84b195593cf5210389fe8a4e04904f13d9f98dcc
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-12 23:47:16 +0530

    PosNegZero

---
+++ PosNegZerojava.java
@@ -0,0 +1,6 @@
+public class PosNegZerojava {
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.println(n == 0 ? "Zero" : (n > 0 ? "Positive" : "Negative"));
+    }
+}
