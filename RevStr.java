19064d41f4c095f7bdeaa0cf196657fe53a150ed
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-12 23:46:27 +0530

    RevStr

---
+++ RevStr.java
@@ -0,0 +1,7 @@
+ public class RevStr {
+    public static void main(String[] args) {
+        String s = "Astha";
+        for (int i = s.length() - 1; i >= 0; i--)
+            System.out.print(s.charAt(i));
+    }
+} 
