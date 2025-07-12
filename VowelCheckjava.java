d0176c9227be0fb9e8699644cfc0428d87c1e938
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-12 23:45:10 +0530

    VowelCheck

---
+++ VowelCheckjava.java
@@ -0,0 +1,6 @@
+public public class VowelCheckjava {
+    public static void main(String[] args) {
+        char ch = 'e';
+        System.out.println("aeiouAEIOU".indexOf(ch) != -1 ? "Vowel" : "Not Vowel");
+    }
+} 
