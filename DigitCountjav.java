15ea6ff0e7e7e441de51f39f5790e6293df8e93b
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-15 23:45:56 +0530

    DigitCount

---
+++ DigitCountjav.java
@@ -0,0 +1,7 @@
+public public class DigitCountjav {
+    public static void main(String[] args) {
+        int n = 12345;
+        int digits = (int) Math.log10(n) + 1;
+        System.out.println("Digits: " + digits);
+    }
+} 
