95bc249fa6f4008fb5dbb734225920a6dd7599ea
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-18 00:02:47 +0530

    PalindromicSubString

---
+++ PalindromicSubstring.java
@@ -0,0 +1,21 @@
+ public class PalindromicSubstring {
+    public static void main(String[] args) {
+        String str = "babad";
+        int n = str.length();
+        boolean[][] dp = new boolean[n][n];
+        String longest = "";
+
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                int j = i + l - 1;
+                if (str.charAt(i) == str.charAt(j) && (l < 3 || dp[i+1][j-1])) {
+                    dp[i][j] = true;
+                    if (l > longest.length())
+                        longest = str.substring(i, j+1);
+                }
+            }
+        }
+
+        System.out.println("Longest Palindromic Substring: " + longest);
+    }
+} 
