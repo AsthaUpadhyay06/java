4252ad9172364d85f1373563be676a32daa3c3df
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-19 23:51:14 +0530

    ReplaceSpace

---
+++ ReplaceSpace.java
@@ -0,0 +1,7 @@
+public class ReplaceSpace {
+    public static void main(String[] args) {
+        String s = "Java is Powerful";
+        s = s.replace(" ", "-");
+        System.out.println("After replacing spaces: " + s);
+    }
+}
