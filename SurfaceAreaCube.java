288b8470d7647f0447aec3dbb2ea10d72906bbb3
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-07-29 23:51:51 +0530

    SurfaceAreaCube

---
+++ SurfaceAreaCube.java
@@ -0,0 +1,7 @@
+public class SurfaceAreaCube {
+    public static void main(String[] args) {
+        int side = 5;
+        int surfaceArea = 6 * side * side;
+        System.out.println("Surface Area of Cube: " + surfaceArea);
+    }
+}
