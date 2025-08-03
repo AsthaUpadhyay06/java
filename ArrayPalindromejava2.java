54d8d21b9aa6f6d98e2390974fb22f6b06f0366c
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-03 23:55:26 +0530

    ArrayPalindrome

---
+++ ArrayPalindromejava2.java
@@ -0,0 +1,13 @@
+ public class ArrayPalindromejava2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        boolean isPalin = true;
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                isPalin = false;
+                break;
+            }
+        }
+        System.out.println(isPalin ? "Palindrome Array" : "Not Palindrome");
+    }
+}
