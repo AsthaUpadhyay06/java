1ca11ba2990849a0e6152dcad764c94172355532
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-03 23:53:01 +0530

    SwapFirstLastDigit

---
+++ SwapFirstLastDigit.java
@@ -0,0 +1,11 @@
+public class SwapFirstLastDigit {
+    public static void main(String[] args) {
+        int n = 12345;
+        String s = String.valueOf(n);
+        char[] a = s.toCharArray();
+        char temp = a[0];
+        a[0] = a[a.length - 1];
+        a[a.length - 1] = temp;
+        System.out.println("After swap: " + new String(a));
+    }
+}
