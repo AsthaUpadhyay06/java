c9c9c43954a62e924c31a2cfb3facd1969e44ef3
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-05 23:57:35 +0530

    PrimeCountInArray

---
+++ PrimeCountInArrayjava.java
@@ -0,0 +1,16 @@
+ public class PrimeCountInArrayjava {
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 7, 10, 11};
+        int count = 0;
+        for (int num : arr)
+            if (isPrime(num)) count++;
+        System.out.println("Prime Count: " + count);
+    }
+}
