15284ed4abb7d44e7e69c23ab44482904bee1bd2
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-07 23:51:48 +0530

    SumDigitInString

---
+++ SumDigitsInStringjava.java
@@ -0,0 +1,12 @@
+public class SumDigitsInStringjava {
+    public static void main(String[] args) {
+        String input = "abc12xy3z";
+        int sum = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sum += Character.getNumericValue(c);
+            }
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
+} 
