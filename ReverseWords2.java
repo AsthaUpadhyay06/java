3b1c91421c9aa3125b33e88c3074341a1970eae4
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-08 23:58:43 +0530

    ReverseWord

---
+++ ReverseWords2.java
@@ -0,0 +1,12 @@
+ public class ReverseWords2 {
+    public static void main(String[] args) {
+        String sentence = "Java is super fun";
+        String[] words = sentence.split(" ");
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]).append(" ");
+        }
+        System.out.println("Reversed: " + reversed.toString().trim());
+    }
+} 
