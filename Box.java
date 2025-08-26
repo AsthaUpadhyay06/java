b2cb262c98c55529304e741188cfba623bbc112d
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-26 23:58:37 +0530

    GenericExample

---
+++ Box.java
@@ -0,0 +1,22 @@
+public class Box<T> {
+    private T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+    public T get() {
+        return value;
+    }
+}
+
+public class GenericsExamplejava {
+    public static void main(String[] args) {
+        Box<Integer> intBox = new Box<>();
+        intBox.set(100);
+        System.out.println("Integer Value: " + intBox.get());
+
+        Box<String> strBox = new Box<>();
+        strBox.set("Hello Generics");
+        System.out.println("String Value: " + strBox.get());
+    }
+} 
