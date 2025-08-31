51df826453b42df0ecbc6a3bb9e6ebbc6714bca6
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-08-31 23:49:43 +0530

    PrimeCheck

---
+++ gfghththgh.java
@@ -0,0 +1,28 @@
+ import java.util.Scanner;
+
+public class ArrayMinMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0], min = arr[0];
+
+        for (int num : arr) {
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+
+        sc.close();
+    }
+} 
